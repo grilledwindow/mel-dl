26d7a65a08260102c1a266d49799159465473d6a
Xavier Ang <xaveang@gmail.com>
2021-06-21 05:02:09 +0800

    Update Module

--- src/mel.rs
+++ src/mel.rs
@@ -1,36 +1,26 @@
 use std::path::Path;
 
-pub enum Modules {
-    AppliedAnalytics,
-    WebAppDev,
-    MobileAppDev,
-    Calculus,
-}
-
 pub struct Module {
-    pub module: Modules,
     pub course_nth: u8,
     pub learning_materials_tab_id: String,
     pub learning_materials_nth: u8,
-    pub download_structure: &'static[&'static str],
+    pub max_depth: u8,
     pub download_path: &'static Path,
 }
 
 impl Module {
     pub fn new(
-        module: Modules,
         course_nth: u8,
         learning_materials_tab_id: &'static str,
         learning_materials_nth: u8,
-        download_structure: &'static[&'static str],
+        max_depth: u8,
         download_path: &'static Path,
     ) -> Self {
         Self {
-            module,
             course_nth,
             learning_materials_tab_id: format!("paletteItem:_{}_1", learning_materials_tab_id),
             learning_materials_nth,
-            download_structure,
+            max_depth,
             download_path,
         }
     }
