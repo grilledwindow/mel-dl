b6cf8ed50ee41aab65df1bfc60288d43b634e3f9
Xavier Ang <xaveang@gmail.com>
2021-06-21 13:10:03 +0800

    Add comments on fields

--- src/mel.rs
+++ src/mel.rs
@@ -1,11 +1,11 @@
 use std::path::Path;
 
 pub struct Module {
-    pub course_nth: u8,
-    pub learning_materials_tab_id: String,
-    pub learning_materials_nth: u8,
-    pub max_depth: u8,
-    pub download_path: &'static Path,
+    pub course_nth: u8,                    // the order of the course on the website
+    pub learning_materials_tab_id: String, // the tab id for the course's learning materials tab
+    pub learning_materials_nth: u8,        // the order of the first week's learning materials
+    pub max_depth: u8,                     // the maximum depth to search for files within folders
+    pub download_path: &'static Path,      // the download path
 }
 
 impl Module {
@@ -24,7 +24,6 @@ impl Module {
             download_path,
         }
     }
-
 }
 /*
     MeL
