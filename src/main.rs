66c714fafdbcfbba09d1b8cbb05a8bce7f62a5df
Xavier Ang <xaveang@gmail.com>
2021-06-30 02:28:58 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,29 @@ mod config;
 use config::Config;
 
 mod login;
+mod directory;
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
+    const URL: &str = "https://mel.np.edu.sg/auth-saml/saml/login?apId=_155_1&redirectUrl=https://mel.np.edu.sg/ultra";
+    let download_path = Path::new("C:\\Users\\xa\\Desktop\\.mel-dl");
+
     let mut caps = DesiredCapabilities::chrome();
-    let v: Value = serde_json::from_str(
-        r#"{
-        "download.default_directory": "C:\\Users\\xa\\Downloads",
-        "download.prompt_for_download": false
-    }"#,
-    )?;
+    
+    // specify the temporary download location.
+    // the files will be moved into a specific folder later.
+    let v: Value = serde_json::json!({
+        "download.prompt_for_download": false,
+        "download.default_directory": download_path.to_string_lossy(),
+    });
+
     caps.add_chrome_option("prefs", v)?;
 
+    // specify the binary path
     caps.add_chrome_option(
         "binary",
         "C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
     )?;
-
-    const URL: &str = "https://mel.np.edu.sg/auth-saml/saml/login?apId=_155_1&redirectUrl=https://mel.np.edu.sg/ultra";
 
     let mut driver = WebDriver::new("http://localhost:4444", &mut caps).await?;
 
@@ -52,8 +57,8 @@ async fn main() -> WebDriverResult<()> {
     driver.sign_in().await?;
 
     let applied_analytics = Module::new(1, 1, Path::new("C:\\Users\\xa\\Desktop\\AA"), true);
-    let config = Config::new().set_folder_no(None);
-    
+    let config = Config::new().set_folder_no(1); // setting to 0 gets currernt week's files
+
     driver.download_files(&applied_analytics, &config).await?;
 
     Ok(())
